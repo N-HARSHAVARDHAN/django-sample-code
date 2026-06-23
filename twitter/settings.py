bf041027cb5bdc509ba828fd17c5e7641f911dde
harsha <harshavardhan53595@gmail.com>
2026-06-23 13:52:40 +0530

    switch to postgres config

--- twitter/settings.py
+++ twitter/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://django_sample_project_postgres_user:TAHUvQGX81JkFICRMaPyBMWLvvBSJ01w@dpg-d8t3ui4m0tmc73bsdvr0-a.oregon-postgres.render.com/django_sample_project_postgres')
+    )
 }
 
 
